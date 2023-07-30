734b6d6dcd70d0339480f747077dfcb2ea2ef990
viceroy <wsrf.liu@qq.com>
2023-07-30 17:34:52 +0800

    增加天车状态页面

---
+++ src/views/dashboard/overhead-crane-state/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <Breadcrumb :items="['仪表盘', '天车状态']" />
+    <div class="content"> 我是内容 </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style scoped lang="less">
+.container {
+  padding: 0 20px 20px 20px;
+}
+</style>
