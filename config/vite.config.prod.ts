1b35962808e29db1057df84e6e396de576e0062c
刘沛 <>
2023-09-20 15:44:36 +0800

    修改生产环境地址

--- config/vite.config.prod.ts
+++ config/vite.config.prod.ts
@@ -6,8 +6,8 @@ export default mergeConfig(
     mode: 'production',
     define: {
       'process.env': {
-        BASE_API: 'http://localhost:8000', // 生产环境
-        WS_API: 'ws://localhost:8000/ws',
+        BASE_API: 'http://192.168.8.8:8000', // 生产环境
+        WS_API: 'ws://192.168.8.8:8000/ws',
         THREE_URL: 'http://192.168.8.8:81', // 三维模型
       },
     },
